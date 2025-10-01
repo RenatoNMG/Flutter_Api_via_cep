618daaad0b4048e918b19dc803b62111afc62fb9
Renato <renatonm9@gmail.com>
2025-09-30 22:07:21 -0300

    função buscar endereço

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:cosumodeapi/Models/endereco.dart';
+import 'package:cosumodeapi/Services/via_cep_service.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -20,66 +23,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController controllerCep = TextEditingController();
+  TextEditingController controllerLogradouro = TextEditingController();
+  TextEditingController controllerLocalidade = TextEditingController();
+  TextEditingController controllerComplemento = TextEditingController();
+  TextEditingController controllerUf = TextEditingController();
+  TextEditingController controllerEstado = TextEditingController();
+  Endereco? endereco; // variavel pode sreceber null;
 
-  void _incrementCounter() async {
-    await Future.delayed(Duration(seconds: 1));
+  ViaCepService viaCepService = ViaCepService();
+
+  Future<void> buscarCep(String cep) async {
+    Endereco? response = await viaCepService.buscarEndereco(cep);
+
+    if (response?.localidade == null) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            icon: Icon(Icons.warning),
+            title: Text("Atenção"),
+            content: Text("Cep Não Encontrado"),
+          );
+        },
+      );
+      return;
+    }
     setState(() {
-      _counter++;
+      endereco = response;
     });
+  }
+
+  void setControllersCep(Endereco endereco) {
+    controllerLogradouro.text = endereco.logradouro!;
+    controllerLocalidade.text = endereco.localidade!;
+    controllerComplemento.text = endereco.complemento!;
+    controllerUf.text = endereco.uf!;
+    controllerEstado.text = endereco.estado!;
   }
 
   @override
   Widget build(BuildContext context) {
-    _incrementCounter();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          spacing: 20,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Center(
+              child: TextField(
+                controller: controllerCep,
+                maxLength: 8,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      buscarCep(controllerCep.text);
+                    },
+                    icon: Icon(Icons.search),
+                  ),
+                  border: OutlineInputBorder(),
+                  labelText: "CEP",
+                ),
+              ),
+            ),
+            Center(
+              child: TextField(
+                controller: controllerLogradouro,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Logradouro",
+                ),
+              ),
+            ),
+            Center(
+              child: TextField(
+                controller: controllerLocalidade,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Localidade",
+                ),
+              ),
+            ),
+            TextField(
+              controller: controllerComplemento,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "complemento",
+              ),
+            ),
+
+            TextField(
+              controller: controllerUf,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "uf",
+              ),
+            ),
+            TextField(
+              controller: controllerEstado,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "estado",
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/Services/via_cep_service.dart
+++ lib/Services/via_cep_service.dart
@@ -4,16 +4,16 @@ import 'package:cosumodeapi/models/endereco.dart';
 import 'package:http/http.dart' as http;
 
 class ViaCepService {
-  Future<Endereco?>  buscarendereco(String cep) async{
+  Future<Endereco?> buscarEndereco(String cep) async {
     String endpoint = "viacep.com.br/ws/$cep/json/";
     Uri uri = Uri.parse(endpoint);
 
-    var response =  await http.get(uri);
-    
-    if (response.statusCode == 200){
+    var response = await http.get(uri);
+
+    if (response.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(response.body);
       return Endereco.fromjson(json);
     }
-
+    return null;
   }
 }
